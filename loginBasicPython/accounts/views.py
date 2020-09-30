582404f63d31dcaf6e6745ab12d52850b3bc21ab
AnkushKhatri1306 <dhhrruvkhatri@gmail.com>
2020-09-30 14:06:11 +0530

    added account api

--- loginBasicPython/accounts/views.py
+++ loginBasicPython/accounts/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    try:
+        print('hi,,')
+        return render(request, "base.html")
+    except Exception as e:
+        print(e.args)
+
+def login(request):
+    try:
+        print('login')
+    except Exception as e:
+        print(e.args)
+
+
+def register(request):
+    try:
+        print('signup')
+    except Exception as e:
+        print(e.args)
